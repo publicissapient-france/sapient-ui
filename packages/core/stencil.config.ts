4d12c4acbb0c3833d0302571737ea95a66910303
Thomas Champion <tchampion@xebia.fr>
2020-11-08 18:47:55 +0100

    Add React output target and generate a React wrapper lib

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -1,4 +1,5 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'core',
@@ -17,5 +18,9 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
+    reactOutputTarget({
+      componentCorePackage: 'sapient-ui-core',
+      proxiesFile: '../react/src/components.ts',
+    }),
   ],
 };
